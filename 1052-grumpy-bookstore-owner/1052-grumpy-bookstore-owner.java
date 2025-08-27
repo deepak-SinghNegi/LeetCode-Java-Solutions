99e7c681bbedc41f9342dd394c69097a822aec69
Deepak Negi <deepaknegi16360@gmail.com>
2025-08-27 16:34:52 +0530

    Time: 3 ms (91.71%), Space: 48 MB (18.95%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,32 +1,26 @@
 class Solution {
-    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-        int totalStfCus = 0;
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes){
+        int sutisfiedCust = 0;
         for(int i=0;i<customers.length;i++){
             if(grumpy[i]==0){
-                totalStfCus+=customers[i];
+                sutisfiedCust+=customers[i];
+            customers[i]=0;
             }
         }
-        int left = 0;
-        int maxUnStfCus = 0;
-        int UnStfCus = 0;
-        for(int right = 0;right<customers.length;right++){
+        int left=0;
+        int sumOfUnstisfiedCust=0;
+        int sum=0;
+        for(int right=0;right<customers.length;right++){
             
             if(grumpy[right]==1){
-                UnStfCus+=customers[right];
-                
+                sum+=customers[right];
             }
-            
-            while(right - left +1>minutes){
-                if(grumpy[left]==1){
-                    UnStfCus-=customers[left];
-                    left++;
-                }
-                else left++;
-                
+            if(right-left+1==minutes){
+                sumOfUnstisfiedCust = Math.max(sumOfUnstisfiedCust,sum);
+                sum-=customers[left++];
+
             }
-            maxUnStfCus = Math.max(maxUnStfCus,UnStfCus);
         }
-            totalStfCus= totalStfCus+maxUnStfCus;
-       return totalStfCus; 
+        return sutisfiedCust+sumOfUnstisfiedCust;
     }
 }
