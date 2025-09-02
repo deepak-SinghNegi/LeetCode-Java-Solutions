b0a97d68a271ef4e8decfd7a469aacf5adf4bf46
Deepak Negi <deepaknegi16360@gmail.com>
2025-09-02 22:50:21 +0530

    Time: 0 ms (100%), Space: 40.9 MB (77.65%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,14 @@
-
-import java.util.Scanner;
-
 class Solution {
-    public int robbing(int[] nums , int i){
+    public int robbing(int[] nums , int i, int[]Dp){
         if(i>=nums.length)return 0;
-        int steal =nums[i] + robbing(nums, i+2); 
-        int skip = robbing(nums, i+1); 
-        return Math.max(skip,steal);
+        if(Dp[i]!=-1) return Dp[i];
+        int steal =nums[i] + robbing(nums, i+2,Dp); 
+        int skip = robbing(nums, i+1,Dp); 
+        return Dp[i] = Math.max(skip,steal);
     }
     public int rob(int[] nums) {
-        // int [] Dp = new int[nums.length+1];
-        // Arrays.fill(Dp, -1);
-        return robbing(nums, 0);
+        int [] Dp = new int[nums.length+1];
+        Arrays.fill(Dp, -1);
+        return robbing(nums, 0,Dp);
     }
 }
-
-
