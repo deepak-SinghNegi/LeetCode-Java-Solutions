51b42b4bd8d035641132a72480b01b35b1d555aa
Deepak Negi <deepaknegi16360@gmail.com>
2025-09-03 19:18:12 +0530

    Time: 2 ms (83.67%), Space: 45.6 MB (71.55%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,26 @@
 class Solution {
-    boolean ans(int[] Dp,int [] nums,int indx){
-        if(indx>=nums.length-1) return true;
-        if(nums[indx]==0) return false;
-        if(Dp[indx]!=-1) return Dp[indx]==1;
-        for(int i=1;i<=nums[indx];i++){
-            if(ans(Dp,nums,indx+i)){
-                Dp[indx]=1;
-                return true;
-            }
-        }
-        return false;
-    }
+    // boolean canComplet(int []Dp,int indx,int[] nums){
+    //     if(indx>=nums.length-1)return true;
+    //     if(nums[indx]==0) return false;
+    //     if(Dp[indx]!=-1) return Dp[indx]==1;
+    //     for(int i=1;i<=nums[indx];i++){
+    //        if (canComplet(Dp,indx+i,nums)){
+    //         Dp[indx] = 1;
+    //         return true;
+    //        }
+    //     }
+    //     return false ;
+    // }
     public boolean canJump(int[] nums) {
-        int[] Dp = new int [nums.length];
-        Arrays.fill(Dp,-1);
-        return ans(Dp,nums,0);
+        // int [] Dp = new int [nums.length];
+        // Arrays.fill(Dp,-1);
+        // return canComplet(Dp , 0,nums) ;
+        int maxReach = 0;
+        for(int i=0;i<nums.length;i++){
+            if(maxReach<i) return  false;
+            maxReach = Math.max(maxReach,nums[i]+i);
+            if(maxReach>=nums.length-1) return true;
+        }
+        return true;
     }
 }
