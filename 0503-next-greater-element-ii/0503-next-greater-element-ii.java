4997320b13c8a35f1ebfbd93726b4c6b079af3ce
Deepak Negi <deepaknegi16360@gmail.com>
2026-02-27 15:30:45 +0530

    Time: 5 ms (93.92%), Space: 48.1 MB (83.01%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,24 +1,18 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
-
- 
+        int[] nge = new int[n];
+        Deque<Integer> st = new ArrayDeque<>();
         for (int i = n - 1; i >= 0; i--) {
-
-       
-            while (!st.isEmpty() && st.peek() <= nums[i]) {
+            st.push(i);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] <= nums[i]) {
                 st.pop();
             }
-
-
-            ans[i] = st.isEmpty() ? -1 : st.peek();
-
-        
-            st.push(nums[i]);
+            nge[i] = st.isEmpty() ? -1 : nums[st.peek()];
+            st.push(i);
         }
-
-        return ans;
+        return nge;
     }
 }
