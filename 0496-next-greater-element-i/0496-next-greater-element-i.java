152b34f50561942e08e9171f08bd56e9ee2fdf45
Deepak Negi <deepaknegi16360@gmail.com>
2026-02-27 14:57:29 +0530

    Time: 4 ms (73.4%), Space: 45.6 MB (9.72%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,31 +1,25 @@
-class Solution{
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int n = nums2.length;
+        int m = nums1.length;
+        int[] nge = new int[n];
+        int[] ans = new int[m];
+        Arrays.fill(nge, -1);
+        Deque<Integer> st = new ArrayDeque<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-    public int[] nextGreaterElement(int nums1[] ,int[] nums2) {
-        Stack<Integer> st = new Stack<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
-        st.push(nums2[nums2.length-1]);
-        int [] nextGr = new int[nums2.length];
-        nextGr[nextGr.length-1] = -1;
-        int [] ans =  new int [nums1.length];
+        for (int i = n - 1; i >= 0; i--) {
 
-        for(int i = nums2.length-2; i>= 0;i--){
-            while(!st.isEmpty()&&st.peek()<=nums2[i]){
+            map.put(nums2[i], i);
+
+            while (!st.isEmpty() && nums2[st.peek()] <= nums2[i]) {
                 st.pop();
-
             }
-
-
-            if(!st.isEmpty())nextGr[i] = st.peek();
-            else nextGr[i] = -1;
-            st.push(nums2[i]);
-
-
+            nge[i] = st.isEmpty() ? -1 : nums2[st.peek()];
+            st.push(i);
         }
-        for(int i = 0; i<nums2.length;i++){
-            map.put(nums2[i],nextGr[i]);
-        }
-        for(int i=0;i<nums1.length;i++){
-            ans[i] = map.get(nums1[i]);
+        for (int i = 0; i < m; i++) {
+            ans[i] = nge[map.get(nums1[i])];
         }
         return ans;
     }
