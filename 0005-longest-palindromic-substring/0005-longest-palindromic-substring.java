da853c5ff958a0a43010f0578b1d77ebd8092a3f
Deepak Negi <deepaknegi16360@gmail.com>
2026-03-04 13:42:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,28 +1,20 @@
 class Solution {
-    private int  isPallindrom(String s,int i,int j,int [][] Dp){
-        if(i>=j) return 1;
-        if(Dp[i][j] != -1) return Dp[i][j];
-        if(s.charAt(i)==s.charAt(j)) return Dp[i][j] = isPallindrom(s, i+1, j-1,Dp);
-        else{
-            return Dp[i][j]=0;
-        }
-        
-    }
     public String longestPalindrome(String s) {
-        int [][]Dp = new int[s.length()+1][s.length()+1];
-        for(int[] arr : Dp){
-            Arrays.fill(arr,-1);
-        }
-        String longest = "";
-        for(int i=0;i<s.length();i++){
-            for(int j=s.length()-1;j>=0;j--){
-                if(isPallindrom(s, i, j,Dp)==1){
-                    if(j-i+1>longest.length())longest = s.substring(i, j+1);
-                   
-                }
-                
+        String ans = "";
+        for(int i = 0;i<s.length();i++){
+            for(int j = i+1 ; j<=s.length();j++){
+                String ss = s.substring(i , j);
+             int x =0;
+             int y =ss.length()-1;
+             while(x<y && ss.charAt(x) == ss.charAt(y)){
+                x++;
+                y--;
+             }
+             if(x>=y && ss.length()>ans.length()){
+                ans =ss;
+             }
             }
         }
-        return longest;
+        return ans;
     }
 }
