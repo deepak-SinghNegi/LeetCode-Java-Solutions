eb015b2a794f17f3ffeca4b4eb8693729e43011f
Deepak Negi <deepaknegi16360@gmail.com>
2026-03-09 21:06:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
@@ -1,21 +1,28 @@
 class Solution {
     public List<String> validStrings(int n) {
         List<String> ans =  new ArrayList<>();
-        helper("" , ans , '1' , n);
+        StringBuilder sb = new StringBuilder();
+        helper(sb , ans , '1' , n);
         return ans;
         
     }
 
-    public void helper(String s, List<String> list, char prev, int n) {
-        if (s.length() == n) {
-            list.add(s);
+    public void helper(StringBuilder sb, List<String> list, char prev, int n) {
+        if (sb.length() == n) {
+            list.add(sb.toString());
             return;
         }
         if (prev == '0') {
-            helper(s + '1', list, '1', n);
+            sb.append('1');
+            helper(sb, list, '1', n);
+            sb.deleteCharAt(sb.length()-1);
             return ;
         }
-        helper(s + '0', list, '0', n);
-        helper(s + '1', list, '1', n);
+        sb.append('0');
+        helper(sb , list, '0', n);
+        sb.deleteCharAt(sb.length()-1);
+        sb.append('1');
+        helper(sb, list, '1', n);
+        sb.deleteCharAt(sb.length()-1);
     }
 }
