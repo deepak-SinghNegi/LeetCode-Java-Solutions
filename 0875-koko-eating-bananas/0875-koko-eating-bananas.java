a4d1c05fb30fd7569ba7fc12b7d653d7c23fc4d0
Deepak Negi <deepaknegi16360@gmail.com>
2026-03-13 21:13:29 +0530

    Time: 6 ms (99.65%), Space: 48.2 MB (23.85%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,45 +1,32 @@
 class Solution {
-    public boolean isValid(int[] piles, int h,int k){
-        int spendHr = 0;
-        int i=0;
-        
-        while(i<piles.length){
-            if(piles[i]/k==0){
-                spendHr++;
-                i++;
-            }
-            else{
-                if(piles[i]%k==0){
-                    spendHr += (piles[i]/k);
-                    i++;
-                }
-                else{
-                    spendHr += (piles[i]/k)+1;
-                    i++;
-                }
-            }
-        }
-        if(spendHr<=h)return true;
-        return false;
-    }
     public int minEatingSpeed(int[] piles, int h) {
-        int minSpeed = 1;
-        int maxSpeed = Integer.MIN_VALUE;
-        for(int speed : piles){
-            // minSpeed = Math.min(minSpeed,speed);
-            maxSpeed = Math.max(maxSpeed,speed);
+        int left = 1;
+        int right = -1;
+        for (int val : piles) {
+            right = Math.max(right, val);
         }
-        int ans = -1;
-        while(minSpeed<=maxSpeed){
-            int midSpeed = minSpeed + (maxSpeed-minSpeed)/2;
-            if(isValid( piles , h , midSpeed)){
-                ans = midSpeed;
-                maxSpeed = midSpeed-1;
-            }
-            else{
-                minSpeed = midSpeed+1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canEat(mid, piles, h)) {
+
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return ans;
+        return right;
+    }
+
+    private boolean canEat(int k, int[] arr, int h) {
+        int hr = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            hr += (arr[i] + k - 1) / k;
+            if (hr > h)
+                return false;
+
+        }
+        return true;
     }
 }
