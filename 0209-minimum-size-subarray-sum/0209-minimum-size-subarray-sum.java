9593443264e5c83ff91ec0cc3650d37fceda2c0d
Deepak Negi <deepaknegi16360@gmail.com>
2026-03-14 23:02:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,36 +1,15 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int l = 1;
-        int r = nums.length;
-        int[] prifix = new int[nums.length];
-        prifix[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            prifix[i] = nums[i] + prifix[i - 1];
-        }
-        if(prifix[r-1] < target) return 0;
-
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-            if (canSub(prifix, mid, target)) {
-                r = mid;
-            } else {
-                l = mid + 1;
-            }
-        }
-        
-        return r;
-    }
-
-    private boolean canSub(int[] prifix, int mid, int target) {
+        int l = 0;
         int sum = 0;
-        for (int i = 0; i + mid <= prifix.length; i++) {
-            int r = i + mid - 1;
-            sum = i == 0 ? prifix[r] : prifix[r] - prifix[i - 1];
-            if (sum >= target) {
-                return true;
+        int minLen = -1;
+        for (int r = 0; r < nums.length; r++) {
+            sum += nums[r];
+            while (sum >= target) {
+                minLen = r - l + 1;
+                sum -= nums[l++];
             }
-
         }
-        return false;
+        return minLen == -1 ? 0 : minLen;
     }
 }
