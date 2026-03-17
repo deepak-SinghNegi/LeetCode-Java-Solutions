bb6d6d2918c8dbed083abc9d45ee5ba90035dbaa
Deepak Negi <deepaknegi16360@gmail.com>
2026-03-17 18:21:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
@@ -20,14 +20,7 @@ class Solution {
            
 
         }
-        for (int i = 0; i < m; i++) {
-
-            for (int j = 0; j < n; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-
-        }
+       
 
       
         return max;
