ba82ea79f503a7cf70f1a2435ffb94476881dc35
Deepak Negi <deepaknegi16360@gmail.com>
2026-03-19 20:10:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -2,39 +2,27 @@ class Solution {
     public int numberOfSubmatrices(char[][] grid) {
         int rows = grid.length;
         int columns = grid[0].length;
-        int[][] preX = new int[rows][columns];
-        preX[0][0] = grid[0][0] == 'X' ? 1 : 0;
-        int[][] preY = new int[rows][columns];
-        preY[0][0] = grid[0][0] == 'Y' ? 1 : 0;
-        int xCount = 0;
-        for (int i = 0; i < rows; i++) {
+        int[][] preX = new int[rows + 1][columns + 1];
 
-            for (int j = 0; j < columns; j++) {
-                if (i == 0 && j == 0)
-                    continue;
-                if (i == 0) {
-                    preX[i][j] = grid[i][j] == 'X' ? preX[i][j - 1] + 1 : preX[i][j - 1];
-                    preY[i][j] = grid[i][j] == 'Y' ? preY[i][j - 1] + 1 : preY[i][j - 1];
-                } else {
-                    if (j == 0) {
-                        preX[i][j] = grid[i][j] == 'X' ? preX[i - 1][j] + 1 : preX[i - 1][j];
-                        preY[i][j] = grid[i][j] == 'Y' ? preY[i - 1][j] + 1 : preY[i - 1][j];
-                    } else {
-                        preX[i][j] = grid[i][j] == 'X' ? preX[i - 1][j] + preX[i][j - 1] - preX[i - 1][j - 1] + 1
-                                : preX[i - 1][j] + preX[i][j - 1] - preX[i - 1][j - 1];
-                        preY[i][j] = grid[i][j] == 'Y' ? preY[i - 1][j] + preY[i][j - 1] - preY[i - 1][j - 1] + 1
-                                : preY[i - 1][j] + preY[i][j - 1] - preY[i - 1][j - 1];
-                    }
-                }
+        int[][] preY = new int[rows + 1][columns + 1];
+
+        int xCount = 0;
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= columns; j++) {
+                preX[i][j] = grid[i - 1][j - 1] == 'X' ? preX[i - 1][j] + preX[i][j - 1] - preX[i - 1][j - 1] + 1
+                        : preX[i - 1][j] + preX[i][j - 1] - preX[i - 1][j - 1];
+                preY[i][j] = grid[i - 1][j - 1] == 'Y' ? preY[i - 1][j] + preY[i][j - 1] - preY[i - 1][j - 1] + 1
+                        : preY[i - 1][j] + preY[i][j - 1] - preY[i - 1][j - 1];
 
             }
 
         }
         int count = 0;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                if ((i == 0 && j == 0) || preX[i][j] == 0 || preY[i][j] == 0)
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                if ((i == 1 && j == 1) || preX[i][j] == 0 || preY[i][j] == 0)
                     continue;
                 if (preX[i][j] == preY[i][j])
                     count++;
