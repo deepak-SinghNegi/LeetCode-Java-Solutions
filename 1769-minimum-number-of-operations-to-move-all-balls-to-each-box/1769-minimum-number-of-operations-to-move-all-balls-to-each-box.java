c9a163404d8f437c3411678e3a9e6e908206e817
Deepak Negi <deepaknegi16360@gmail.com>
2026-04-04 12:52:39 +0530

    Time: 134 ms (20.14%), Space: 46.8 MB (25.31%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,34 +1,14 @@
 class Solution {
     public int[] minOperations(String boxes) {
         int n = boxes.length();
-        int [] prifix = new int [n];
-        int x = 1;
-        int balls =0;
-        while(x<n &&boxes.charAt(x-1)-'0'==0)x++;
-
-        
-        for(int i=x;i<n;i++){
-           if(boxes.charAt(i-1)-'0'==1)balls++;
-            
-                
-                prifix[i] =  prifix[i-1]+balls;
-            
+        int [] ans = new int [n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j<n;j++){
+                if(boxes.charAt(j) != '0'){
+                    ans[i] += Math.abs(j - i);
+                }
+            }
         }
-        x = n-2;
-        int [] sufix = new int [n];
-        while(x>=0 && boxes.charAt(x+1)-'0'==0)x--;
-        balls = 0;
-        for(int i=x;i>=0;i--){
-           if (boxes.charAt(i+1)-'0'==1)balls++;
-            
-                
-                sufix[i] =  sufix[i+1]+balls;
-            
-        }
-       
-        for(int i=0;i<n;i++){
-            prifix[i]+=sufix[i];
-        }
-        return prifix;
+        return ans;
     }
 }
