9b6aef1cf9a6a5d3f26233e889606562e86049d9
Deepak Negi <deepaknegi16360@gmail.com>
2026-04-18 19:32:29 +0530

    Time: 7 ms (39.42%), Space: 44.9 MB (95.65%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,37 +1,29 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> st = new Stack<>();
-        for (String s : tokens) {
-            int temp;
-            switch (s) {
-                case "+":
-                     temp = st.pop();
-                    temp += st.peek();
-                    st.pop();
-                    st.push(temp);
-                    break;
-                case "-":
-                     temp = st.pop();
-                    temp = st.peek() - temp;
-                    st.pop();
-                    st.push(temp);
-                    break;
-                case "*":
-                     temp = st.pop();
-                    temp *= st.peek();
-                    st.pop();
-                    st.push(temp);
-                    break;
-                case "/":
-                     temp = st.pop();
-                   temp =  st.peek()/ temp;
-                   st.pop();
-                    st.push(temp);
-                    break;
-                default:
-                    st.push(Integer.parseInt(s));
-
+        st.push(Integer.parseInt(tokens[0]));
+        int i = 0;
+        while(i < tokens.length){
+            if(!(tokens[i].equals("+") || tokens[i].equals("-")||tokens[i].equals("*")|| tokens[i].equals("/"))){
+                st.push(Integer.parseInt(tokens[i]));
             }
+            else{
+                int x = st.pop();
+                int y = st.pop();
+                if(tokens[i].equals("+")){
+                    st.push(x + y);
+                }
+                else if(tokens[i].equals("-")){
+                    st.push(y - x);
+                }
+                else if(tokens[i].equals("*")){
+                    st.push(x * y);
+                }
+                else{
+                    st.push( y/x);
+                }
+            }
+            i++;
         }
         return st.peek();
     }
