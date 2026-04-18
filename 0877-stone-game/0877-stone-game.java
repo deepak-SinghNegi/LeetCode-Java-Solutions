f2487df8b155cc9dd46ce2bc122dcaec55401c0f
Deepak Negi <deepaknegi16360@gmail.com>
2026-04-18 12:47:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,22 +1,21 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i< piles.length; i++){
-            list.add(piles[i]);
-        }
-        int a = 0;
-        int b = 0;
         
-        while(list.size()>0){
-            int indx = list.get(0) > list.get(list.size() - 1) ? 0 : list.size() - 1;
-            a += list.get(indx);
-            list.remove(indx);
-            if(list.size() <= 0) break;
-            indx = list.get(0) > list.get(list.size() - 1) ? 0 : list.size() - 1;
-            b += list.get(indx);
+        int a = 0;
+        int i = 0;
+        int j = piles.length - 1;
+        int sum = 0;
+        for(int x = 0; x<= j; x++){
+            sum += piles[x];
         }
 
-        return a > b;
+        
+        while(i < j){
+           int indx = piles[i] > piles[j] ? i++ : j --; 
+           a += piles[indx];
+        }
+
+        return a > sum/2 ;
         
     }
 }
