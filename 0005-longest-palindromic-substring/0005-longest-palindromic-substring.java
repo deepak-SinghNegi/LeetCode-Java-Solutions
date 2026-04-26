1e9b9d90848e176bb189adf3a7d2d27ba67cca79
Deepak Negi <deepaknegi16360@gmail.com>
2026-04-26 22:41:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,24 +1,31 @@
 class Solution {
-    private boolean  isPallindrom(String s,int i,int j,boolean [][] Dp){
-        if(i>=j) return true;
-        if(Dp[i][j] ) return Dp[i][j];
-        if(s.charAt(i)==s.charAt(j)) return Dp[i][j] = isPallindrom(s, i+1, j-1,Dp);
-        else{
-            return Dp[i][j]=false;
+
+    private boolean isPalindrome(String s, int i, int j, Boolean[][] dp) {
+        if (i >= j) return true;
+
+        if (dp[i][j] != null) return dp[i][j];
+
+        if (s.charAt(i) == s.charAt(j)) {
+            return dp[i][j] = isPalindrome(s, i + 1, j - 1, dp);
         }
-        
+
+        return dp[i][j] = false;
     }
+
     public String longestPalindrome(String s) {
-        boolean [][]Dp = new boolean[s.length()+1][s.length()+1];
-       
+        int n = s.length();
+        Boolean[][] dp = new Boolean[n][n];
+
         String longest = "";
-        for(int i=0;i<s.length();i++){
-            for(int j=s.length()-1;j>=0;j--){
-                if(isPallindrom(s, i, j,Dp)){
-                    if(j-i+1>longest.length())longest = s.substring(i, j+1);
-                   
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= i; j--) {
+
+                if (isPalindrome(s, i, j, dp)) {
+                    if (j - i + 1 > longest.length()) {
+                        longest = s.substring(i, j + 1);
+                    }
                 }
-                
             }
         }
         return longest;
