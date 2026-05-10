c561d48df68e7e26f8b3f53cf9f42001f404be59
Deepak Negi <deepaknegi16360@gmail.com>
2026-05-10 21:25:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.java
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.java
@@ -1,24 +1,17 @@
 class Solution {
     public int maximumJumps(int[] nums, int target) {
         int n = nums.length;
-        int[] Dp = new int[n];
-        Arrays.fill(Dp, -2);
-        Dp[n - 1] = 0;
-        return helper(nums, Dp, target, 0);
-    }
+        Integer Dp[] = new Integer[n];
 
-    private int helper(int[] nums, int[] Dp, int t, int i) {
-        if (Dp[i] != -2)
-            return Dp[i];
-        int ans = -1;
-        for (int j = i + 1; j < nums.length; j++) {
-            if (Math.abs(nums[i] - nums[j]) <= t) {
-                int next = helper(nums, Dp, t, j);
-                if (next != -1) {
-                    ans = Math.max(ans, 1 + next);
+        Dp[n - 1] = 0;
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (Dp[j] != null && Math.abs(nums[i] - nums[j]) <= target) {
+                    Dp[i] = Dp[i] == null ? Dp[j] + 1 : Math.max(Dp[i], Dp[j] + 1);
                 }
             }
         }
-        return Dp[i] = ans;
+        return Dp[0] == null ? -1 : Dp[0];
     }
 }
