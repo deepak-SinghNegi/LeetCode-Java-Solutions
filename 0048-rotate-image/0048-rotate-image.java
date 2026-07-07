2fbc380b0d256451587a5b8323ef223bb3f8c4af
Deepak Negi <deepaknegi16360@gmail.com>
2026-07-07 23:15:59 +0530

    Time: 0 ms (100%), Space: 43.7 MB (52.84%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -8,12 +8,7 @@ class Solution {
               matrix[j][i] = temp;
             }
         }
-        for(int i = 0; i< n; i++){
-            for(int j =0; j<n;j++){
-              System.out.print(matrix[i][j]);
-            }
-            System.out.println();
-        }
+        
         for(int i = 0; i< n; i++){
             for(int j =0; j<n/2;j++){
               int temp = matrix[i][j];
