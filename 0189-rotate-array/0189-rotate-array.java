6f18698d18240e67af1e9b1d0b4311c4f08e03ed
Deepak Negi <deepaknegi16360@gmail.com>
2026-07-12 11:14:27 +0530

    Time: 2 ms (19.8%), Space: 61.1 MB (70.71%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,25 +1,36 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int [] ans = new int [nums.length];
-        while(nums.length<k){
-            k = Math.abs(nums.length-k);
+        int n = nums.length;
+        int f = 0;
+        int r = n - 1;
+        while(r>=f){
+            int temp = nums[f];
+            nums[f] = nums[r];
+            nums[r] = temp;
+            f++;
+            r--;
         }
-        int i = nums.length-k;
-        int j = 0;
-        if(nums.length<=1)return;
-        while(i<nums.length){
-            ans[j] = nums[i];
-            i++;
-            j++;
+        
+        k %=n;
+        f = 0;
+        r = k-1;
+         while(r>f){
+            int temp = nums[f];
+            nums[f] = nums[r];
+            nums[r] = temp;
+            f++;
+            r--;
         }
-        i =0;
-        while(i<nums.length-k){
-            ans[j] = nums[i];
-            i++;
-            j++;
+        
+        f = k;
+        r = n-1;
+        while(r >=f){
+         int temp = nums[f];
+            nums[f] = nums[r];
+            nums[r] = temp;
+            f++;
+            r--;
         }
-        for(int l =0;l<ans.length;l++){
-        nums[l] = ans[l];
-       }
+       
     }
 }
