9179c50d9aeadedd17b25926246be78aa78b04e4
Deepak Negi <deepaknegi16360@gmail.com>
2026-07-17 18:51:37 +0530

    Time: 11 ms (97.27%), Space: 54.2 MB (26.09%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.java
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -1,15 +1,32 @@
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
         int n = intervals.length;
-        TreeMap<Integer , Integer> map = new TreeMap<>();
-        for(int i = 0; i<n;i++){
-            map.put(intervals[i][0] , i);
+        int [][] starts = new int[n][2];
+        int [] res = new int [n];
+        for(int i =0; i<n;i++){
+            starts[i][0] = intervals[i][0];
+            starts[i][1] = i;
+           
         }
-        int ans[] = new int [n];
-        for(int i = 0; i<n;i++){
-            int end = intervals[i][1];
-            ans [i] = map.ceilingEntry(end) ==null ? -1 :map.ceilingEntry(end).getValue() ;
+        Arrays.sort(starts , (a,b)->a[0]-b[0]);
+        for(int i = 0;i<n;i++){
+            int target = intervals[i][1];
+            int l = 0;
+            int r = n-1;
+            int ans = -1;
+            while(l<=r){
+                int m = l + (r-l)/2;
+                if(starts[m][0] >= target){
+                    ans = starts[m][1];
+                    r = m - 1;
+                }
+                else {
+                    l = m + 1;
+                }
+            }
+            res[i] = ans;
         }
-        return ans;
+
+        return res;
     }
 }
