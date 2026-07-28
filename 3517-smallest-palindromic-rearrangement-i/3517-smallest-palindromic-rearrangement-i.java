82e214ab5be3c8ee0785a9be78d7734d6d4ace57
Deepak Negi <deepaknegi16360@gmail.com>
2026-07-28 18:44:29 +0530

    Time: 30 ms (70.68%), Space: 53.5 MB (10.04%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,24 +1,23 @@
 class Solution {
     public String smallestPalindrome(String s) {
         int freq[] = new int[26];
-
-        for (char ch : s.toCharArray()) {
+        int n = s.length();
+        for (int i = 0; i<n/2;i++) {
+            char ch = s.charAt(i);
             freq[ch - 'a']++;
         }
         String mid = "";
-        if (s.length() % 2 == 1) {
-            mid += s.charAt(s.length() / 2);
+        if (n % 2 == 1) {
+            mid += s.charAt(n/2);
         }
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < 26; i++) {
+            
             char ch = (char) ('a' + i);
-            int c = freq[ch - 'a'] / 2;
-            while (c > 0) {
-                c--;
+            while(freq[ch - 'a']-- >0)
                 sb.append(ch);
-                
-            }
+            
         }
         return sb.toString() +mid+sb.reverse().toString();
     }
