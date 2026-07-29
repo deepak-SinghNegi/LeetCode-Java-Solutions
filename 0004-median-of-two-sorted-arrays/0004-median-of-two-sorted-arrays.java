0da429323798986ccd37a715ae2f37883c1c7db1
Deepak Negi <deepaknegi16360@gmail.com>
2026-07-29 12:00:52 +0530

    Time: 1 ms (100%), Space: 48.8 MB (52.83%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,30 +1,35 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        if (m < n) {
-            return findMedianSortedArrays(nums2, nums1);
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        if(n2 < n1){
+            return findMedianSortedArrays(nums2 , nums1);
         }
         int low = 0;
-        int high = n;
+        int high = n1 ;
+        while(low <= high){
+            int n1Part = low + (high - low)/2;
+            int n2Part = (n1 + n2 + 1) / 2 - n1Part;
+            int nums1Left = n1Part == 0 ? min : nums1[n1Part - 1];
+            int nums1Right = n1Part >= n1 ? max : nums1[n1Part];
+            int nums2Left = n2Part == 0 ? min : nums2[n2Part - 1]; 
+            int nums2Right = n2Part >= n2 ? max : nums2[n2Part];
 
-        while (low <= high) {
-            int partitionX = low + (high - low) / 2;
-            int partitionY = (m + n + 1) / 2 - partitionX;
-            int leftS = partitionX == 0 ? Integer.MIN_VALUE : nums1[partitionX - 1];
-            int rightS = partitionX >= n ? Integer.MAX_VALUE : nums1[partitionX];
-            int leftL = partitionY == 0 ? Integer.MIN_VALUE : nums2[partitionY - 1];
-            int rightL = partitionY == m ? Integer.MAX_VALUE : nums2[partitionY];
-            if (leftS <= rightL && leftL <= rightS) {
-                if ((m + n) % 2 != 0)
-                    return Math.max(leftS, leftL);
-                else {
-                    return (Math.max(leftS, leftL) + Math.min(rightS, rightL)) / 2.0;
+            if(nums1Left <= nums2Right && nums2Left <= nums1Right){
+                if((n1 + n2) % 2 != 0){
+                    return Math.max(nums1Left , nums2Left);
                 }
-            } else if (leftS > rightL) {
-                high = partitionX - 1;
-            } else {
-                low = partitionX + 1;
+                else{
+                    return (Math.max(nums1Left , nums2Left) + Math.min(nums1Right , nums2Right))/2.0;
+                }
+            } 
+            else if(nums1Left > nums2Right){
+                high = n1Part - 1;
+            }
+            else{
+                low = n1Part + 1;
             }
         }
         return 0.0;
