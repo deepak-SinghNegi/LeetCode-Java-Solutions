cd95418a7ec73b1dc7eac8a4a0015d1079e988bf
Deepak Negi <deepaknegi16360@gmail.com>
2026-07-31 16:05:06 +0530

    Time: 1 ms (48.37%), Space: 43.4 MB (64.32%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,6 +1,8 @@
 class Solution {
-    public List<String> letterCombinations(String digits) {
         HashMap<Character, String> map = new HashMap<>();
+        int dgtLen = 0;
+    public List<String> letterCombinations(String digits) {
+        dgtLen = digits.length();
         map.put('2', "abc");
         map.put('3', "def");
         map.put('4', "ghi");
@@ -10,63 +12,21 @@ class Solution {
         map.put('8', "tuv");
         map.put('9', "wxyz");
         List<String> res = new ArrayList<>();
-        String s1 = "";
-        String s2 = "";
-        String s3 = "";
-        String s4 = "";
-        Stack<String> st = new Stack<>();
-        for (char ch : digits.toCharArray()) {
-            if (s1.isEmpty())
-                s1 = map.get(ch);
-            else if (s2.isEmpty())
-                s2 = map.get(ch);
-            else if (s3.isEmpty())
-                s3 = map.get(ch);
-            else
-                s4 = map.get(ch);
+        StringBuilder sb = new StringBuilder();
+        lcUtil(res , sb ,digits , 0 );
+        return res;
+    }
+    private void lcUtil(List<String> res ,StringBuilder sb,String digits , int idx){
+        if(idx >= dgtLen){
+            res.add(sb.toString());
+            return;
         }
-        if (s2.isEmpty()) {
-            for (int i = 0; i < s1.length(); i++) {
-                res.add(String.valueOf(s1.charAt(i)));
-            }
-
+        String s = map.get(digits.charAt(idx));
+        for(int i =0; i<s.length();i++){
+            
+            sb.append(s.charAt(i));
+            lcUtil(res , sb , digits , idx+1);
+            sb.deleteCharAt(sb.length()-1);
         }
-        for (int i = 0; i < s1.length(); i++) {
-            for (int j = 0; j < s2.length(); j++) {
-                String s = String.valueOf(s1.charAt(i)) + String.valueOf(s2.charAt(j));
-                res.add(s);
-            }
-        }
-        if (s3.isEmpty())
-            return res;
-        else if (s4.isEmpty()) {
-            List<String> res1 = new ArrayList<>();
-            for (int i = 0; i < res.size(); i++) {
-                for (int j = 0; j < s3.length(); j++) {
-                    String s = res.get(i) + s3.charAt(j);
-                    res1.add(s);
-                }
-            }
-            return res1;
-        } else {
-            List<String> res2 = new ArrayList<>();
-            for (int i = 0; i < s3.length(); i++) {
-                for (int j = 0; j < s4.length(); j++) {
-                    String s = String.valueOf(s3.charAt(i)) + String.valueOf(s4.charAt(j));
-                    res2.add(s);
-                }
-
-            }
-            List<String> finalop = new ArrayList<>();
-
-            for (int i = 0; i < res.size(); i++) {
-                for (int j = 0; j < res2.size(); j++) {
-                    finalop.add(res.get(i) + res2.get(j));
-                }
-            }
-            return finalop;
-
-        }
-
     }
 }
