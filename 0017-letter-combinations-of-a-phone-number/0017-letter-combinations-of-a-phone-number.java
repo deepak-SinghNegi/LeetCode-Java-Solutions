98505448416b2aa01ddefec8c0b54d4e9096c40d
Deepak Negi <deepaknegi16360@gmail.com>
2026-07-31 16:13:10 +0530

    Time: 0 ms (100%), Space: 43.6 MB (38.1%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,32 +1,38 @@
 class Solution {
-        HashMap<Character, String> map = new HashMap<>();
-        int dgtLen = 0;
+
+    int dgtLen = 0;
+    String[] map;
     public List<String> letterCombinations(String digits) {
         dgtLen = digits.length();
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
+        map = new String[]{
+            "",
+            "",
+            "abc",
+            "def",
+            "ghi",
+            "jkl",
+            "mno",
+            "pqrs",
+            "tuv",
+            "wxyz"
+        };
         List<String> res = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        lcUtil(res , sb ,digits , 0 );
+        lcUtil(res, sb, digits, 0);
         return res;
     }
-    private void lcUtil(List<String> res ,StringBuilder sb,String digits , int idx){
-        if(idx >= dgtLen){
+
+    private void lcUtil(List<String> res, StringBuilder sb, String digits, int idx) {
+        if (idx >= dgtLen) {
             res.add(sb.toString());
             return;
         }
-        String s = map.get(digits.charAt(idx));
-        for(int i =0; i<s.length();i++){
-            
+        String s = map[digits.charAt(idx) -'0'];
+        for (int i = 0; i < s.length(); i++) {
+
             sb.append(s.charAt(i));
-            lcUtil(res , sb , digits , idx+1);
-            sb.deleteCharAt(sb.length()-1);
+            lcUtil(res, sb, digits, idx + 1);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
