5011eadf92205a0125d711526673df1b7cb2839f
Deepak Negi <deepaknegi16360@gmail.com>
2026-07-31 17:26:01 +0530

    Time: 8 ms (99.71%), Space: 48.3 MB (17.34%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -10,37 +10,31 @@ class CharAndFreq {
 
 class Solution {
     public int minimumPushes(String word) {
-        PriorityQueue<CharAndFreq> pq = new PriorityQueue<>((a, b) -> b.count - a.count);
         int[] freq = new int[26];
-        int totalPush = 0;
-        int unCount = 0;
-        int push = 1;
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
             freq[ch - 'a']++;
         }
-        for (int i = 0; i < 26; i++) {
+        Arrays.sort(freq);
+        int totalPress = 0;
+        int unChar = 0;
+        for (int i = 25; i >= 0; i--) {
             char ch = (char) ('a' + i);
             if (freq[i] > 0) {
-                System.out.println(ch + " -> " + freq[i]);
-                pq.add(new CharAndFreq(ch, freq[i]));
+                
+                if(unChar <8)
+                    totalPress += freq[i];
+                else if(unChar <16)
+                    totalPress += freq[i]*2;
+                else if(unChar <24)
+                    totalPress += freq[i]*3;
+                else
+                    totalPress += freq[i]*4;
+                unChar++;
             }
 
         }
-        int totalPress = 0;
-        int i = 0;
-        while (!pq.isEmpty()) {
-            if (i < 8) {
-                totalPress += pq.poll().count;
-            } else if (i < 16) {
-                totalPress += pq.poll().count * 2;
-            } else if (i < 24) {
-                totalPress += pq.poll().count * 3;
-            } else {
-                totalPress += pq.poll().count * 4;
-            }
-            i++;
-        }
+        
 
         return totalPress;
     }
