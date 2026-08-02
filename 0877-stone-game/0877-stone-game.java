aeee1e87097164c07a1ab967f1f2435f7302d984
Deepak Negi <deepaknegi16360@gmail.com>
2026-08-02 10:50:26 +0530

    Time: 6 ms (22.55%), Space: 46.9 MB (13.54%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,21 +1,23 @@
 class Solution {
+    int[][] Dp ;
     public boolean stoneGame(int[] piles) {
-        
-        int a = 0;
-        int i = 0;
-        int j = piles.length - 1;
+        int n = piles.length;
         int sum = 0;
-        for(int x = 0; x<= j; x++){
-            sum += piles[x];
+        for (int stn : piles) {
+            sum += stn;
         }
+        Dp = new int [n+1][n+1];
+        int alice = helper(piles, 0, n - 1);
+        return alice > sum - alice;
+    }
 
-        
-        while(i < j){
-           int indx = piles[i] > piles[j] ? i++ : j --; 
-           a += piles[indx];
+    private int helper(int[] piles, int i, int j) {
+        if (i > j) {
+            return 0;
         }
-
-        return a > sum/2 ;
-        
+        if(Dp[i][j] != 0) return Dp[i][j];
+        int s_take = piles[i] + Math.min(helper(piles, i + 2, j), helper(piles, i + 1, j - 1));
+        int e_take = piles[j] + Math.min(helper(piles, i + 1, j - 1), helper(piles, i, j - 2));
+        return Dp[i][j] =  Math.max(s_take, e_take);
     }
 }
