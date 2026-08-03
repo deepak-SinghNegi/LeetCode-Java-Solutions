75f99c9a13c2ee7eab83768779097ad478093dea
Deepak Negi <deepaknegi16360@gmail.com>
2026-08-03 12:54:13 +0530

    Time: 5 ms (38.05%), Space: 45 MB (39.18%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if ((ch <= 'z' && ch >= 'a') || (ch <= 'Z' && ch >= 'A') || (ch >= '0' && ch <= '9')) {
+                if (ch < 97) {
+                    ch = (char) (ch + 32);
+                }
+                sb.append(ch);
+            }
+        }
+        int l = 0;
+        int r = sb.length() - 1;
+        while (l <= r) {
+            if (sb.charAt(l++) != sb.charAt(r--))
+                return false;
+        }
+        System.out.println(sb);
+        return true;
+    }
+}
