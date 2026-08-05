92b5a4f69c70f7baf405efc338cf059130937e1c
Deepak Negi <deepaknegi16360@gmail.com>
2026-08-05 22:31:56 +0530

    Time: 90 ms (38.46%), Space: 284.1 MB (58.65%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.java
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -4,51 +4,58 @@ class Solution {
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
+
         Set<Integer> set = new HashSet<>();
+
         for (int i = 0; i < invo.length; i++) {
             int u = invo[i][0];
             int v = invo[i][1];
 
             graph.get(u).add(v);
         }
+
         boolean[] visited = new boolean[n];
+
         Queue<Integer> q = new LinkedList<>();
+
         q.offer(k);
         visited[k] = true;
-        while (!q.isEmpty()) {
 
-            int size = q.size();
+        while (!q.isEmpty()) {
             int curr = q.poll();
             set.add(curr);
-            
+
             for (int neig : graph.get(curr)) {
                 if (!visited[neig]) {
                     visited[neig] = true;
                     q.offer(neig);
                 }
-
             }
-
-            
         }
+
         boolean flag = false;
+
         for (int i = 0; i < invo.length; i++) {
             int u = invo[i][0];
             int v = invo[i][1];
+
             if (!set.contains(u) && set.contains(v)) {
                 flag = true;
                 break;
             }
 
         }
+
         List<Integer> res = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
             if (flag)
                 res.add(i);
             else if (!set.contains(i))
                 res.add(i);
         }
-        return res;
-    }
 
+        return res;
+
+    }
 }
