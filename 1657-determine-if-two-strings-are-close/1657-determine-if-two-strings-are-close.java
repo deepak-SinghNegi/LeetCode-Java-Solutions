f15e649d769fbfea5b2f9dacf2ce46bde8532746
Deepak Negi <deepaknegi16360@gmail.com>
2026-08-07 19:25:02 +0530

    Time: 10 ms (98.24%), Space: 47.8 MB (70.02%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -0,0 +1,24 @@
+class Solution {
+ 
+
+    public boolean closeStrings(String word1, String word2) {
+        int n1 = word1.length();
+        int n2 = word2.length();
+        int freq1[] = new int[26];
+        int freq2[] = new int[26];
+        for (char c : word1.toCharArray()) {
+            freq1[c - 'a']++;
+        }
+        for (char c : word2.toCharArray()) {
+            freq2[c - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i]==0) != (freq2[i]==0))
+                return false;
+        }
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+        return Arrays.equals(freq1, freq2);
+
+    }
+}
