bed5b4fda139cb52b5d29d324484019f8c2dba96
Deepak Negi <deepaknegi16360@gmail.com>
2026-09-10 21:52:23 +0530

    Time: 485 ms (5.08%), Space: 273.2 MB (5.58%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -12,14 +12,9 @@ class Solution {
             return 0;
         }
         if(Dp[i][j+1] != null) return Dp[i][j+1];
-        if(j < 0){
-            int c1 = 1 + helper(nums, i + 1, i);
-            int c2 = helper(nums, i + 1, j);
-            return Dp[i][j+1] =  Math.max(c1, c2);
-        }
-        
+      
 
-        else if (nums[j] < nums[i]) {
+        if (j == -1 || nums[j] < nums[i]) {
             int c1 = 1 + helper(nums, i + 1, i);
             int c2 = helper(nums, i + 1, j);
             return Dp[i][j+1] =  Math.max(c1, c2);
